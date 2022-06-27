6d53d7c1c27c60c11521a55fdbbe67c7c7f472dc
Olang <oluoch9@gmail.com>
2022-06-27 22:23:44 +0300

    .

---
+++ credentials.py
@@ -0,0 +1,14 @@
+from os import access
+
+
+# Twitter Credentials
+
+consumer_key = "bQOSmEyAPo2ywe21KXrErlW1h"
+consumer_secret = "YKhevE2JD9IW83yyzPBgXR45QnvmUTzCM9djGXl54LHzqSYhus"
+access_token = "993737466-1cwkpORyF3UlFaE5xyjtMyOyXjjvaSgsxQrRYw79"
+access_token_secret = "6XnbyVvFO1bUnRYTG8wBXFMdOoa24Tt8SBaY7aG9r8N2B"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAANNpOgEAAAAApWbTVDyDjTNTq6RsVwXjWGm41Mg%3DamIwItkuTCrCA1vCn5nfgUC0Hr9CyN77NJaZ0EzOYswfXFHcky"
+
+# Postrgres Credentials
+user = "postgres"
+password = "abstract"
