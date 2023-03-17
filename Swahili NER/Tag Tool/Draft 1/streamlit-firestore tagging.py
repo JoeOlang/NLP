bc4191bf3ed4d33352396476e90b1e619437fb26
Emmanuel Olang <64926283+e-olang@users.noreply.github.com>
2023-03-17 13:41:29 +0300

    .

--- Swahili NER/Tag Tool/Draft 1/streamlit-firestore tagging.py
+++ Swahili NER/Tag Tool/Draft 1/streamlit-firestore tagging.py
@@ -1,12 +1,21 @@
 import streamlit as st
-import pandas as pd
 import random
-import os
+import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 # ------------------------------------------------------------------------------------
-# CONSTANTS
+# FIRESTORE
+cred = credentials.Certificate("Credentials/ner-tag-tool-firebase-adminsdk-8as43-e118b9010b.json")
 
-df_file = 'sdata.csv'
-sentence_file = 'RFI April - Novmber 2022 titles - cleaned.txt'
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# CONSTANTS
+#df_file = 'sdata.csv'
+sentence_file = 'Data/RFI April - Novmber 2022 titles - cleaned.txt'
 
 # ------------------------------------------------------------------------------------
 # Read txt file with sentences, without '\n'
@@ -33,7 +42,6 @@ def main():
     # Define the list of items to process
     sent , items = random_selection()
 
-
     # title and sentence
     st.markdown("<h1 style='text-align: center;'>NER Taging Tool</h1>", unsafe_allow_html=True)
     st.markdown("-----")
@@ -42,13 +50,6 @@ def main():
     st.markdown(f"<p style='text-align: justify;'>{sent}</p>", unsafe_allow_html=True)
     st.markdown("-----")
 
-    if not os.path.isfile(df_file):
-        # Create a new dataframe
-        df = pd.DataFrame(columns=['Sentence', 'Tags'])
-    else:
-        # load existing data
-        df = pd.read_csv(df_file)
-
     # define the dropdown options
     dropdown_options = ["", "PERSON", "NORP", "FAC", "ORG", "GPE", "LOC", "PRODUCT", "EVENT", "WORK_OF_ART", "LAW", 
                         "LANGUAGE", "DATE", "TIME", "PERCENT", "MONEY", "QUANTITY", "ORDINAL", "CARDINAL"]
@@ -56,10 +57,11 @@ def main():
     # Create a list to store new tags, this will extend the existing data
     
 
-    
     # Create a form for user inputs
     with st.form(key='tag_form'):
+
         new_tags = []
+
         for i, item in enumerate(items):
             st.write(f"Enter an NER Tag for: {item}:")
             value = st.selectbox("Select a tag", dropdown_options, key=i)
@@ -68,20 +70,28 @@ def main():
         # Add a submit button to the form
         submitted = st.form_submit_button('Submit')
 
-        # If the form is submitted, append the new tags to the dataframe
+        # If the form is submitted, write the new tags to Firestore
         if submitted:
-            df = df.append({'Sentence': sent, 'Tags': new_tags}, ignore_index=True)
+            # Define the Firestore document ID as a combination of the sentence and a timestamp
+            doc_id = f"{sent}_{int(time.time())}"
+            
+            # Define the document data
+            doc_data = {
+                'sentence': sent,
+                'tags': new_tags
+            }
+            
+            # Write the document to Firestore
+            db.collection('ner_tags').document(doc_id).set(doc_data)
 
-            # Save the dataframe
-            df.to_csv(df_file, index=False)
             st.success("Data saved successfully!")
-    
-    # if the form is not submitted, display the current tags
-    if not submitted:
-        for tag in new_tags:
-            for key, value in tag.items():
-                st.write(f"{key}: {value}")
 
+            # Reset the form
+            new_tags = []
+
+    st.markdown('---')
+    st.write("For reference, the possible tags are described below:")
+    st.write("PERSON: People, including fictional.", "NORP: Nationalities or religious or political groups.", "FAC: Buildings, airports, highways, bridges, etc.", "ORG: Companies, agencies, institutions, etc.", "GPE: Countries, cities, states.", "LOC: Non-GPE locations, mountain ranges, bodies of water.", "PRODUCT: Objects, vehicles, foods, etc. (Not services.)", "EVENT: Named hurricanes, battles, wars, sports events, etc.", "WORK_OF_ART: Titles of books, songs, etc.", "LAW: Named documents made into laws.", "LANGUAGE: Any named language.", "DATE: Absolute or relative dates or periods.", "TIME: Times smaller than a day.", "PERCENT: Percentage, including ”%“.", "MONEY: Monetary values, including unit.", "QUANTITY: Measurements, as of weight or distance.", "ORDINAL: “first”, “second”, etc.", "CARDINAL: Numerals that do not fall under another type.")
 
 
 if __name__ == "__main__":
