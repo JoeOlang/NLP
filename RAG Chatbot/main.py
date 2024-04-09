377fe6b1cb41bb76d93d7279be64e1a4105dda14
eolang <64926283+e-olang@users.noreply.github.com>
2024-04-09 11:05:57 +0200

    Revamp.

--- RAG Chatbot/main.py
+++ RAG Chatbot/main.py
@@ -1,3 +1,8 @@
-class Chatbot():
-    # Todo
-    
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+
+loader = TextLoader('./smaple.txt')
+
+
+
